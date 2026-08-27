fa08314f8433b432fafe75e8340a27061ba7aa10
angelmarinmahm <angel.marin.mahm@gmail.com>
2026-08-27 04:34:15 +0000

    Chore: commit de archivo requirements.txt

--- test_app.py
+++ test_app.py
@@ -1,30 +1,30 @@
-from app import es_contrasena_segura as cs
+from app import es_contrasena_segura
 
 def test_contrasena_segura():
-    resultado = cs("SecureP@ss123")
+    resultado = es_contrasena_segura("SecureP@ss123")
     assert resultado["es_segura"] is True
 
 def test_longitud_insegura():
-    resultado = cs("P@s1")
+    resultado = es_contrasena_segura("P@s1")
     assert resultado["es_segura"] is False
     assert resultado["longitud_aceptada"] is False
 
 def test_sin_mayuscula():
-    resultado = cs("securep@ss123")
+    resultado = es_contrasena_segura("securep@ss123")
     assert resultado["es_segura"] is False
     assert resultado["tiene_mayuscula"] is False
 
 def test_sin_minuscula():
-    resultado = cs("SECUREP@SS123")
+    resultado = es_contrasena_segura("SECUREP@SS123")
     assert resultado["es_segura"] is False
     assert resultado["tiene_minuscula"] is False
 
 def test_sin_numero():
-    resultado = cs("SecureP@ss")
+    resultado = es_contrasena_segura("SecureP@ss")
     assert resultado["es_segura"] is False
     assert resultado["tiene_numero"] is False
 
 def test_sin_caracter_especial():
-    resultado = cs("SecurePass123")
+    resultado = es_contrasena_segura("SecurePass123")
     assert resultado["es_segura"] is False
     assert resultado["tiene_caracter_especial"] is False
